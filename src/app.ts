1c0b7d7f29a6d72e1de0a18be166ea4f65fa7725
LiponRoy <liponbdbd@gmail.com>
2024-06-03 13:33:36 +0600

    Implemented API Not Found functionality

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
-import express, { Application } from 'express';
-import cors from 'cors';
-import errorMiddleware from './middlewares/errors';
-import routes from './routes';
+import express, { Application, NextFunction, Request, Response } from "express";
+import cors from "cors";
+import errorMiddleware from "./middlewares/errors";
+import routes from "./routes";
+import httpStatus from "http-status";
+import notFound from "./middlewares/notFound";
 
 const app: Application = express();
 //middlewares
@@ -10,13 +12,16 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 // route
-app.use('/api/v1/', routes);
+app.use("/api/v1/", routes);
 
-app.get('/', (req, res) => {
-	res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 // error middleware ...
 app.use(errorMiddleware);
+
+// Not found error middleware
+app.use(notFound);
 
 export default app;
