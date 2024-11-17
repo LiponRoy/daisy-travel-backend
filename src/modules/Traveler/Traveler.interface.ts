959d587869b24257c131bdea9e0313ca46e69fe1
LiponRoy <liponbdbd@gmail.com>
2024-11-17 23:51:47 +0600

    more merge conflict resolved

--- src/modules/Traveler/Traveler.interface.ts
+++ src/modules/Traveler/Traveler.interface.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export interface IsubTraveler {
 	name: String;
 	email: String;
@@ -6,22 +5,9 @@ export interface IsubTraveler {
 export interface Itraveler {
 	name: String;
 	email: String;
+	pdf1: String;
+	pdf1_cloudinary_id: String;
+	pdf2: String;
+	pdf2_cloudinary_id: String;
 	Travelers: IsubTraveler[];
 }
-=======
-export interface IsubTraveler{
-	name:String,
-	email:String,
-
-}
-export interface Itraveler{
-	name:String,
-	email:String,
-	pdf1:String,
-	pdf1_cloudinary_id:String,
-	pdf2:String,
-	pdf2_cloudinary_id:String,
-	Travelers:IsubTraveler[],
-}
-
->>>>>>> 74ea45429983aa7562bb86eadb0677d6869565f2
